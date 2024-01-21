185bb743196290aa82731ec4ab4e525cc299fa2e
lvit001 <140283164+lvit001@users.noreply.github.com>
2024-01-21 13:41:43 -0600

    fixed function that will adjust the color of the circle, needed to use return instead of color =

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,7 +1,7 @@
 // create the map
 let myMap = L.map("map", {
     center: [37.09, -95.71],
-    zoom: 5
+    zoom: 3
   });
   
   // Adding the tile layer
@@ -17,22 +17,22 @@ let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.ge
 // deeper depth = darker color
 function markerColor(coordinate) {   
     if (coordinate > 90) {
-        color = "darkred";
+        return "darkred";
     }
     else if (coordinate > 70) {
-        color = "red";
+        return "red";
     }
     else if (coordinate > 50) {
-        color = "orangered";
+        return "orangered";
     }
     else if (coordinate > 30) {
-        color = "orange";
+        return "orange";
     }
     else if (coordinate > 10) {
-        color = "yellow";
+        return "yellow";
     }
     else {
-        color = "green";
+        return "green";
     }
 }
 
@@ -44,7 +44,7 @@ function markerSize(magnitude) {
         return 1;
     }
     else {
-        return magnitude * 5;
+        return magnitude * 3;
     }
 } 
 
@@ -58,10 +58,11 @@ d3.json(url).then(data => {
             return L.circleMarker(latlng);
         },
 
+        // set the style of the map using the predefined markerColor and markerSize functions
         style: function(feature) {
             return {
-                // color: "white",
-                // stroke: true,
+                color: "white",
+                stroke: true,
                 fillColor: markerColor(feature.geometry.coordinates[2]),
                 fillOpacity: 0.75,
                 weight: 1,
@@ -70,17 +71,15 @@ d3.json(url).then(data => {
 
         },
         
+        // add popups to each circle on the map
         onEachFeature: function(feature, layer) {
-            layer.bindPopup(`<h3> Location: ${feature.properties.place}</h3><hr><p> Date: ${new Date(feature.properties.time)}</p><hr><p> Magnitude: ${feature.properties.mag}</p>`);
+            layer.bindPopup(`<h3> Location: ${feature.properties.place}</h3><hr><p> 
+            Date: ${new Date(feature.properties.time)}</p><hr><p> Magnitude: ${feature.properties.mag}</p>
+            <hr><p> Depth: ${feature.geometry.coordinates[2]}</p>`);
         }
 
     }).addTo(myMap);
 })
-
-
-
-
-
 
 
 // // Add a legend
